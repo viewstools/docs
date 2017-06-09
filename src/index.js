e473690b762c25aaaef846b9451e4cc12b817d70
Tom Parandyk <tom@uxtemple.com>
2017-06-09 20:38:51 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.view.js';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
